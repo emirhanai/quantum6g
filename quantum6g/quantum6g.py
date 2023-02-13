52ecbebbc5a1803ad3d680dd246a7a1447f3922c
Emirhan BULUT <72056400+emirhanai@users.noreply.github.com>
2023-02-14 01:53:26 +0300

    emirhan_quantum_1.0.0 >>> emirhan_quantum_1.1.0
    emirhan_quantum_1.0.0 >>> emirhan_quantum_1.1.0

--- quantum6g/quantum6g.py
+++ quantum6g/quantum6g.py
@@ -1,34 +1,33 @@
-# quantum6g/quantum6g.py
+import tensorflow as tf
 import pennylane as qml
-import numpy as np
+
+
 class Quantum6G:
-    default_num_qubits = 2
-    dev = qml.device("default.qubit", wires=default_num_qubits)
+    dev = qml.device("default.qubit", wires=2)
+
     @qml.qnode(dev)
-    def quantum_6g_nn(self, weights, x=None):
-        for W in weights:
-            qml.RX(W[0], wires=0)
-            qml.RY(W[1], wires=1)
-            qml.CNOT(wires=[0, 1])
-        return qml.expval(qml.PauliZ(0)), qml.expval(qml.PauliZ(1))
-    def __init__(self, num_qubits):
-        self.num_qubits = num_qubits
-    def fit(self, X, Y, weights, steps=100, learning_rate=0.1):
-        optimizer = qml.AdamOptimizer(learning_rate)
+    def qnode(inputs, weights):
+        qml.AngleEmbedding(inputs, wires=range(2))
+        qml.BasicEntanglerLayers(weights, wires=range(2))
+        return [qml.expval(qml.PauliZ(wires=i)) for i in range(2)]
 
-        for i in range(steps):
-            # Compute the loss for the current set of weights
-            loss = 0.0
-            for x, y in zip(X, Y):
-                y_pred = self.evaluate(weights, x)
-                loss += np.abs(y_pred - y)
+    def build_model(self, X_train, y_train, X_test, y_test):
+        clayer_1 = tf.keras.layers.Dense(1, activation="relu")
 
-            # Update the weights using gradient descent
-            grads = optimizer.compute_gradients(loss, weights)
-            optimizer.apply_gradients(grads)
+        inputs = tf.keras.Input(shape=(1,))
+        x = clayer_1(inputs)
+        x = tf.keras.layers.Dropout(0.1)(x)
 
-    def evaluate(self, weights, x=None):
-        return self.quantum_6g_nn(weights, x)
+        x = qml.qnn.KerasLayer(self.qnode, {"weights": (4, 2)}, output_dim=1)(x)
 
+        model = tf.keras.Model(inputs=inputs, outputs=x)
+        opt = tf.keras.optimizers.SGD(learning_rate=0.2)
+        model.compile(opt, loss="mse", metrics=["accuracy"])
 
+        model_fit = model.fit(X_train, y_train, epochs=2, batch_size=256,
+                              shuffle=True, validation_data=(X_test, y_test),
+                              verbose=2,
+                              callbacks=[tf.keras.callbacks.ModelCheckpoint("/model/model_{epoch}.h5")])
 
+        model.evaluate(X_test, y_test)
+        return model
--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 # setup.py
 from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="quantum6g",
-    version="0.1.0",
+    version="1.1.0",
     description="This library is an automatic artificial intelligence library that combines Quantum and 6G technologies.",
     author="Emirhan BULUT",
     author_email="emirhan@isap.solutions",
     url="https://github.com/emirhanai/quantum6g",
     packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=["pennylane","numpy"],
     keywords='quantum machine-learning, quantum 6g, 6g,quantum,AI, quantum neural networks, qnn, quantum ai, artificial intelligence',
     test_suite='tests',
